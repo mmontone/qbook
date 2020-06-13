c7651b905d634baa0bd87f8cfd9d125e60d19d3f
Mariano Montone <marianomontone@gmail.com>
2020-06-13 18:18:19 -0300

    Custom code info collectors

--- qbook.asd
+++ qbook.asd
@@ -11,9 +11,11 @@
 (defsystem :qbook
   :components ((:static-file "qbook.asd")
                (:module :src
-                :components ((:file "code-analysis" :depends-on ("packages" "qbook"))
-                             (:file "html" :depends-on ("packages" "qbook" "stylesheets"))
-                             (:file "stylesheets" :depends-on ("packages"))
+                :components
+                ((:file "code-analysis" :depends-on ("packages" "qbook"))
+                 (:file "defcode-info-collector" :depends-on ("code-analysis"))
+                 (:file "html" :depends-on ("packages" "qbook" "stylesheets"))
+                 (:file "stylesheets" :depends-on ("packages"))
 			     (:file "latex" :depends-on ("packages" "qbook"))
 			     (:file "packages")
 			     (:file "asdf" :depends-on ("packages" "qbook"))
@@ -25,6 +27,8 @@
 ;;;;@include "src/asdf.lisp"
 
 ;;;;@include "src/code-analysis.lisp"
+
+;;;;@include "src/defcode-info-collector.lisp"
 
 ;;;;@include "src/html.lisp"
 
---
+++ src/defcode-info-collector.lisp
@@ -0,0 +1,33 @@
+;;;; ** Example of a custom code part definition
+
+;;;; This is an example of how to define a custom code-part
+
+(in-package :qbook)
+
+;;;; Define a custom descriptor
+
+(defclass info-collector-descriptor (defun-descriptor)
+  ()
+  (:default-initargs
+   :label-prefix "code-info-collector"
+   :pretty-label-prefix "Code Info Collector"))
+
+;;;; Define an code info collector that instantiate the custom descriptor
+
+(defcode-info-collector defcode-info-collector (name lambda-list &body body)
+  (multiple-value-bind (lambda-list env)
+      (arnesi::walk-lambda-list lambda-list nil nil)
+    (multiple-value-bind (body docstring declarations)
+        (handler-bind ((arnesi::return-from-unknown-block
+                         (lambda (c)
+                           (declare (ignore c))
+                           (invoke-restart 'arnesi::add-block))))
+          (arnesi::walk-implict-progn nil body env :docstring t :declare t))
+      (declare (ignore declarations))
+      (make-instance 'info-collector-descriptor
+                     :name name
+                     :lambda-list lambda-list
+                     :body body
+                     :docstring docstring))))
+
+;; Possibly implement custom HTML and Latex generation.
